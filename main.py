6f5f17b0c5e6ee6d65f8c8cbe7a54d619caa6720
DnBaal <dantheassassin2000@gmail.com>
2025-04-06 17:14:20 +0200

    create moving

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ from player import Player
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -18,8 +26,13 @@ def main():
                 pygame.quit()
                 return
 
+        updatable.update(dt)
+
         screen.fill(color="Black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
